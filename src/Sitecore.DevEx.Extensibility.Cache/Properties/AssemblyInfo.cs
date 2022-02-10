f363f3e50266b2111e6f9bcf0e0c5bdf1b735f96
Bogdan Dorokhin <bogdan94@live.ru>
2022-02-11 01:13:31 +0200

    Updated assembly infos

--- src/Sitecore.DevEx.Extensibility.Cache/Properties/AssemblyInfo.cs
+++ src/Sitecore.DevEx.Extensibility.Cache/Properties/AssemblyInfo.cs
@@ -1,5 +1,4 @@
 ﻿using System.Reflection;
 
-[assembly: AssemblyVersion("4.1.0")]
-[assembly: AssemblyFileVersion("4.1.0")]
-[assembly: AssemblyInformationalVersion("4.1.0")]
+[assembly: AssemblyProduct("Sitecore.DevEx.Extensibility.Cache")]
+[assembly: AssemblyDescription("Sitecore.DevEx.Extensibility.Cache")]
