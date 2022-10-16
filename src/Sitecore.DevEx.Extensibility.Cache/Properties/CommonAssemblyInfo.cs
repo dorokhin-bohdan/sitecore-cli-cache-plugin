63356922a89eaff3b6a7d3afc2e7a5aa831304d5
Bogdan Dorokhin <24988081+dorohin@users.noreply.github.com>
2022-10-16 22:00:14 +0300

    Updated CLI up to 5.1.25

--- src/Sitecore.DevEx.Extensibility.Cache/Properties/CommonAssemblyInfo.cs
+++ src/Sitecore.DevEx.Extensibility.Cache/Properties/CommonAssemblyInfo.cs
@@ -1,7 +1,7 @@
 ﻿using System.Reflection;
 
-[assembly: AssemblyVersion("4.2.0")]
-[assembly: AssemblyFileVersion("4.2.0")]
-[assembly: AssemblyInformationalVersion("4.2.0")]
+[assembly: AssemblyVersion("5.1.25")]
+[assembly: AssemblyFileVersion("5.1.25")]
+[assembly: AssemblyInformationalVersion("5.1.25")]
 [assembly: AssemblyCompany("Bohdan Dorokhin")]
 [assembly: AssemblyCopyright("© 2022 Copyright Bohdan Dorokhin All rights reserved.")]
